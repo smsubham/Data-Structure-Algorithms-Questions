5b3de9cc2fb7635b2838662b2a4d8dfb9fde0f02
smsubham <smsubham342@gmail.com>
2021-06-30 02:34:01 +0530

    Singly Linked List
    Added Codes for Singly Linked List

---
+++ Data Structure/Linked list/SIngly Linked List/Count nodes of linked list .py
@@ -0,0 +1,62 @@
+# https://practice.geeksforgeeks.org/problems/count-nodes-of-linked-list/1
+
+
+'''
+
+#Linked list class
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+        '''
+class Solution:
+    
+    #Function to count nodes of a linked list.
+    def getCount(self, head_node):
+        #code here
+        temp = head_node
+        count =0
+        while temp:
+            temp=temp.next
+            count+=1
+        return count
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+import io
+import sys
+
+    
+# Node class
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+#Linked list class
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+
+    # append at the end of the list
+    def append(self,new_node):
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+            return
+        self.tail.next=new_node
+        self.tail = self.tail.next
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n=int(input())
+        a=LinkedList()
+        nodes=list(map(int,input().strip().split())) #list containing nodes
+        for x in nodes:
+            node=Node(x) # create a new node
+            a.append(node)
+        print(Solution().getCount(a.head))
+# } Driver Code Ends
---
+++ Data Structure/Linked list/SIngly Linked List/Delete a Node in Single Linked List  (2).py
@@ -0,0 +1,95 @@
+# https://practice.geeksforgeeks.org/problems/delete-a-node-in-single-linked-list/1
+
+# your task is to complete this function
+# function should return new head pointer
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+def delNode(head, position):
+    # Code here
+        position = position-1
+    # If linked list is empty
+        if head == None:
+            return
+ 
+        # Store head node
+        temp = head
+ 
+        # If head needs to be removed
+        if position == 0:
+            head = temp.next
+            temp = None
+            return head
+ 
+        # Find previous node of the node to be deleted
+        for i in range(position -1 ):
+            temp = temp.next
+            if temp is None:
+                break
+ 
+        # If position is more than number of nodes
+        if temp is None:
+            return
+        if temp.next is None:
+            return
+ 
+        # Node temp.next is the node to be deleted
+        # store pointer to the next of node to be deleted
+        #next = temp.next.next
+ 
+        # Unlink the node from linked list
+        #temp.next = None
+ 
+        temp.next = temp.next.next
+        return head
+#{ 
+#  Driver Code Starts
+# Node Class    
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+# Linked List Class
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            temp = self.head
+            while(temp.next):
+                temp=temp.next
+            temp.next = new_node
+
+def printlist(head):
+    temp = head
+    while(temp):
+        print(temp.data, end=" ")
+        temp = temp.next
+    print('')
+
+# Driver Program
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        k = int(input())
+        newhead = delNode(list1.head, k)
+        printlist(newhead)
+# Contributed By: Harshit Sidhwa
+# } Driver Code Ends
