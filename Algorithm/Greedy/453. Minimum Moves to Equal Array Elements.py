1ca82b33d9978f75e5b3a0471ae2092264e2653b
smsubham <smsubham342@gmail.com>
2021-08-29 23:14:21 +0530

    .

--- Algorithm/Greedy/453. Minimum Moves to Equal Array Elements.py
+++ Algorithm/Greedy/453. Minimum Moves to Equal Array Elements.py
@@ -1,4 +1,5 @@
 # https://leetcode.com/problems/minimum-moves-to-equal-array-elements/
+# Explanation: https://leetcode.com/problems/minimum-moves-to-equal-array-elements/discuss/93817/It-is-a-math-question
 # Source: https://leetcode.com/problems/minimum-moves-to-equal-array-elements/discuss/272994/Python-Greedy-Sum-Min*Len
 
 class Solution:
